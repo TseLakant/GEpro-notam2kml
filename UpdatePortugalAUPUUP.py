3a2cd4251cee1dd3d34a16f3948581eafdb5e234
TseLakant <bukemail@gmail.com>
2026-02-18 20:41:23 +0500

    Update UpdatePortugalAUPUUP.py

--- UpdatePortugalAUPUUP.py
+++ UpdatePortugalAUPUUP.py
@@ -4,15 +4,16 @@ from bs4 import BeautifulSoup
 import re
 from playwright.sync_api import sync_playwright
 import time
-import sys
 
 
 # --- ПАРАМЕТРЫ ---
+IS_ONLINE = True
 HTML_FILE = "AUP_UUP Details.htm"
 INPUT_KML = "Data Base.kml"
 OUTPUT_KML = "Active Regions.kml"
 KML_NS = 'http://www.opengis.net/kml/2.2' # Правильное пространство имен для GEpro KML 2.2
 FULL_COPY = ["SUPLEMENTOS ACTIVIDADES", "ESPAÇO AÉREO", "AERODROMOS E CAMPOS VOO"]  # Эти папки полетят в файл целиком
+TRACEBACK_FILE = "Traceback.txt"
 
 
 def download_page():
@@ -24,22 +25,29 @@ def download_page():
         page.goto('https://www.public.nm.eurocontrol.int/PUBPORTAL/gateway/spec/')
 
         # Перебираем возможные варианты времени, что бы перейти по ссылке, (каждая попытка +- 2 сек)
-        cur_time = time.localtime()
+        flag = False
+        cur_time = time.gmtime()
         cur_date = '/'.join([str(cur_time.tm_mday + 100)[1:3], str(cur_time.tm_mon + 100)[1:3], str(cur_time.tm_year)])
-        cur_hour = cur_time.tm_hour - 5
-        for trying_time in range(cur_hour * 2 + 2):
+        cur_hour = cur_time.tm_hour
+        for trying_time in range(cur_hour*2):
             cur_time = ':'.join([str(cur_hour - trying_time // 2 + 100)[1:3], ['30', '00'][trying_time % 2]])
             need_page = cur_date + " " + cur_time
             try:
                 # Ожидаем появление новой вкладки (page) после клика
                 with context.expect_page() as new_page_info:
-                    # Кликаем по ссылке/кнопке
+                    # Кликаем по ссылке
                     page.get_by_text(need_page).click(timeout=2000)
+                    print(f"⬇️Downloading EU table: {need_page}")
+                    flag = True
                     break
             except Exception as e:
-                print(f'failed to get page: {need_page}')
+                print(f"❌Dont have EU table: '{need_page}'")
 
-            # Это и есть та самая страница, которая открылась
+        if not flag:
+            print("\n\033[31mError\033[0m, sorry program didn't found EU table\nplease restart the program")
+            input('press enter to exit program...')
+            exit(0)
+        # Это и есть та самая страница, которая открылась
         target_page = new_page_info.value
 
         # Ждем, пока JS отрисует таблицу (networkidle — нет запросов в течение 0.5 сек)
@@ -50,7 +58,7 @@ def download_page():
         with open(HTML_FILE, "w", encoding="utf-8") as f:
             f.write(html_code)
 
-        print(f"HTML код успешно сохранен в файл {HTML_FILE}")
+        print(f"✅Table successfully saved in '{HTML_FILE}'")
         browser.close()
 
 def process_ge_pro_kml(input_path, output_path, folders_to_copy, regions_dict):
@@ -75,10 +83,10 @@ def process_ge_pro_kml(input_path, output_path, folders_to_copy, regions_dict):
         folder_name = name_node.text if name_node is not None else "Unnamed Folder"
 
         if folder_name in folders_to_copy:
-            print(f"📦 Копируем папку полностью: {folder_name}")
+            print(f"📦 Full coping folder: {folder_name}")
             # Ничего не делаем, она остается в дереве со всем содержимым
         else:
-            print(f"🔧 Обработка содержимого папки: {folder_name}")
+            print(f"🔧 Processing folder contents: {folder_name}")
             # ПРИМЕР ОБРАБОТКИ: Удаляем всё, кроме Placemark с конкретным стилем
             # или просто удаляем папку, если она не нужна
 
@@ -87,7 +95,7 @@ def process_ge_pro_kml(input_path, output_path, folders_to_copy, regions_dict):
 
             # Если нужно отфильтровать метки внутри этой папки:
             placemarks = folder.findall("{{{}}}Placemark".format(KML_NS))
-            print(f"Найдено Placemark'ов для обработки: {len(placemarks)} в файле {folder_name}")
+            sorted_pm_count = 0
 
             found_in_kml = set()
 
@@ -105,6 +113,8 @@ def process_ge_pro_kml(input_path, output_path, folders_to_copy, regions_dict):
                     pm_name_normalized = pm_name_normalized.strip().split()[0]
 
                     if pm_name_normalized in regions_dict:
+                        # подсчитывает кол-во оставшихся регионов
+                        sorted_pm_count += 1
 
                         if pm_name_normalized not in found_in_kml:
                             found_in_kml.add(pm_name_normalized)
@@ -141,6 +151,7 @@ def process_ge_pro_kml(input_path, output_path, folders_to_copy, regions_dict):
                         # l_width.text = "2"
                     else:
                         folder.remove(pm)
+            print(f'\tNumber of processed placemarks: {sorted_pm_count}')
 
     # Сохранение с корректным объявлением XML для Google Earth
     with open(output_path, 'wb') as f:
@@ -157,6 +168,7 @@ def parse_eaup_htm(file_path):
     rows = soup.find_all('tr')
     seen_records = set()
     parsed_lp_regions = dict()
+    print('🔍Founded regions:')
     for row in rows:
         cells = row.find_all(['td', 'th'])
         row_text = "|".join([c.get_text(strip=True) for c in cells if c.get_text(strip=True)])
@@ -180,13 +192,13 @@ def parse_eaup_htm(file_path):
                     val = f'FL{val}'
                 altitudes.append(val)
             clean_alts = altitudes[:2]
-            alt_display = '/'.join(clean_alts) if clean_alts else "не указаны"
+            alt_display = '/'.join(clean_alts) if clean_alts else "\033[31mNot specified\033[0m"
             record_key = f"{region_name}|{time_str}|{alt_display}"
             if record_key not in seen_records:
                 seen_records.add(record_key)
                 clean_region_name = region_name[2:].lower().replace('-', '').strip()
                 time_alt_string = f"{time_str} | {alt_display} AMSL"
-                print(clean_region_name, time_alt_string)
+                print('', clean_region_name+' ', time_alt_string, sep='\t')
                 if clean_region_name in parsed_lp_regions:
                     parsed_lp_regions[clean_region_name].append(time_alt_string)
                 else:
@@ -196,23 +208,27 @@ def parse_eaup_htm(file_path):
 
 if __name__ == "__main__":
     try:
-        download_page()
-        print(f"Запуск парсинга {HTML_FILE}...")
+        if IS_ONLINE:
+            download_page()
+        print(f"🔧Start parsing '{HTML_FILE}'...")
         try:
             lp_regions = parse_eaup_htm(HTML_FILE)
         except FileNotFoundError:
-            print(f"Файл {HTML_FILE} не найден")
+            print(f"File '{HTML_FILE}' didn't found, check the file name with database, it must be called '{HTML_FILE}'")
             exit(1)
 
         if lp_regions:
-            print(f"Найдено {len(lp_regions)} активных регионов в HTML-данных.")
+            print(f"🔍Founded {len(lp_regions)} active regions in European AUP/UUP.")
             process_ge_pro_kml(INPUT_KML, OUTPUT_KML, FULL_COPY, lp_regions)
-            print(f"Сохранено в {OUTPUT_KML}.")
+            print(f"✅Saved in '{OUTPUT_KML}'.")
         else:
-            print("Регионы для обновления не найдены, KML не создан.")
-        print("\nПрограмма завершила ошибку без ошибок")
+            print("\033[31mRegions for update didn't found, KML didn't created, try later")
+        print("\n\033[32mProcess finished\033[0m, without errors.")
     except Exception as e:
-        print(f"\nПроизошла ошибка: {e}")
+        print(f"\n\033[31mAn unexpected error occurred\033[0m, wrote in file '{TRACEBACK_FILE}'")
+        with open(TRACEBACK_FILE, 'w', encoding='utf-8') as f:
+            f.write(str(e))
     finally:
-        input("\nНажмите Enter, чтобы закрыть окно...")
+        # Записываю в переменную enter, чтобы избавиться от бага с необходимостью дважды нажимать enter
+        press = input("\nPress enter to exit...")
 
